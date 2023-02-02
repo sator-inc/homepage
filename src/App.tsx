5fbaea78f7b66d7d1c488af036e2103c26ec319f
suecharo <suehiro619@gmail.com>
2023-02-02 15:29:32 +0900

    Update Vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: "800px",
+        color: "#7d7d7d",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "left",
+          padding: "30px",
+        }}
+      >
+        <h1
+          style={{
+            fontSize: "1.8rem",
+          }}
+        >
+          株式会社 Sator
+        </h1>
+        <p
+          style={{
+            margin: "10px 0",
+          }}
+        >
+          研究及び受託開発における実務経験を元に設立された受託開発会社です。
+          大学や研究所、製薬会社などをターゲットとし、システム開発、研究インフラの整備・構築、受託解析などの業務を行っています。
         </p>
+        <p
+          style={{
+            margin: "10px 0",
+          }}
+        >
+          当社は、開発においてコンサルティングから実際の開発までを一貫して行うことで、お客様の要望に応じた最適な解決策を提供します。
+          技術的には、フロントエンド・バックエンド・インフラなど、幅広い技術力を持ち合わせています。
+          また、バイオインフォマティクスや機械学習などの分野における研究経験もあり、迅速かつ確実にお客様の要望に応えることができます。
+        </p>
+        <h2
+          style={{
+            fontSize: "1.4rem",
+            margin: "20px 0 10px",
+          }}
+        >
+          会社情報
+        </h2>
+        <ul>
+          <li>社名: 株式会社 Sator</li>
+          <li>設立: 2023年1月</li>
+          <li>役員: 代表取締役 角崎 太郎</li>
+          <li>資本金: 300万円</li>
+          <li>連絡先: general [at] sator.co.jp</li>
+        </ul>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
